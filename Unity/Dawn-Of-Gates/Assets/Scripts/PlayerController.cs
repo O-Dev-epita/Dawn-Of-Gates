7f9fac3cb8f3fd8453a9da9a3872b5c3bdc09e8c
Gendai <gengendai389@gmail.com>
2015-01-17 20:44:14 +0100

    Correction du projet Dawn-Of-Gates
    J'ai remis comme avant.

---
+++ Unity/Dawn-Of-Gates/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour 
+{
+	public float speed = 3.0F;
+	public float rotateSpeed = 2.0F;
+	public enum RotationAxes { MouseXAndY = 0, MouseX = 1, MouseY = 2 }
+	public RotationAxes axes = RotationAxes.MouseXAndY;
+	public float sensitivityX = 15F;
+	public float sensitivityY = 15F;
+	
+	public float minimumX = -360F;
+	public float maximumX = 360F;
+	
+	public float minimumY = -60F;
+	public float maximumY = 60F;
+	
+	float rotationX = 0F;
+	float rotationY = 0F;
+	
+	Quaternion originalRotation;
+	
+	void Update() {
+		CharacterController controller = GetComponent<CharacterController>();
+		Vector3 forward = -transform.TransformDirection(Vector3.forward);
+		float curSpeed = speed * Input.GetAxis("Vertical");
+		controller.SimpleMove(forward * curSpeed);
+		if (axes == RotationAxes.MouseXAndY)
+		{
+			
+			rotationX += Input.GetAxis("Mouse X") * sensitivityX;
+			rotationY += -Input.GetAxis("Mouse Y") * sensitivityY;
+			
+			rotationX = ClampAngle (rotationX, minimumX, maximumX);
+			rotationY = ClampAngle (rotationY, minimumY, maximumY);
+			
+			Quaternion xQuaternion = Quaternion.AxisAngle (Vector3.up, Mathf.Deg2Rad * rotationX);
+			Quaternion yQuaternion = Quaternion.AxisAngle (Vector3.left, Mathf.Deg2Rad * rotationY);
+			
+			transform.localRotation = originalRotation * xQuaternion * yQuaternion;
+		}
+		else if (axes == RotationAxes.MouseX)
+		{
+			rotationX += Input.GetAxis("Mouse X") * sensitivityX;
+			rotationX = ClampAngle (rotationX, minimumX, maximumX);
+			
+			Quaternion xQuaternion = Quaternion.AxisAngle (Vector3.up, Mathf.Deg2Rad * rotationX);
+			transform.localRotation = originalRotation * xQuaternion;
+		}
+		else
+		{
+			rotationY += -Input.GetAxis("Mouse Y") * sensitivityY;
+			rotationY = ClampAngle (rotationY, minimumY, maximumY);
+			
+			Quaternion yQuaternion = Quaternion.AxisAngle (Vector3.left, Mathf.Deg2Rad * rotationY);
+			transform.localRotation = originalRotation * yQuaternion;
+		}
+	}
+	
+	void Start ()
+	{
+		
+		if (rigidbody)
+			rigidbody.freezeRotation = true;
+		originalRotation = transform.localRotation;
+	}
+	
+	public static float ClampAngle (float angle, float min, float max)
+	{
+		if (angle < -360F)
+			angle += 360F;
+		if (angle > 360F)
+			angle -= 360F;
+		return Mathf.Clamp (angle, min, max);
+	}
+}
