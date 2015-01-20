384560f510140a9147ffb840c0269448f94e715b
obs145628 <obs145628@gmail.com>
2015-01-20 15:06:18 +0100

    Rangement 2.0

---
+++ Unity test/Dawn-Of-Gates/Assets/Scripts/Shuriken.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shuriken : MonoBehaviour 
+{
+	public int speed = 10;
+	void Start()
+	{
+		//transform.rotation = new Quaternion (0f, 0.7059872f, 0.7082247f, 0f);
+		//transform.Rotate (90f, 0f, 0f, Space.World);
+	}
+	void Update () {
+		transform.Translate(Vector3.forward * Time.deltaTime * speed);
+		Destroy (gameObject, 3);
+	}
+}
---
+++ Unity test/Dawn-Of-Gates/Assets/Scripts/Teleportation.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Teleportation : MonoBehaviour {
+	
+	public GameObject otherPortal;
+	public Texture openedPortalTexture;
+	public Texture closedPortalTexture;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	bool teleportOk() {
+		GameState gameState = GameState.Instance;
+		return gameState.leftPortalOpen && gameState.rightPortalOpen;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		renderer.material.mainTexture = teleportOk() ? openedPortalTexture : closedPortalTexture;
+	}
+	
+	void OnTriggerEnter(Collider collider)
+	{
+		Debug.Log(collider.name);
+		if(this.teleportOk())
+		{
+			collider.transform.position = otherPortal.transform.position + otherPortal.transform.forward * 1;
+		}	
+	}
+}
