81da10a50eaf85d03065ea8f0b0863d50cc5ecac
Gendai <gengendai389@gmail.com>
2015-01-20 19:42:52 +0100

    Ajout course et nouvelle tourelle

--- Unity/Dawn-Of-Gates/Assets/Scripts/PlayerController.cs
+++ Unity/Dawn-Of-Gates/Assets/Scripts/PlayerController.cs
@@ -30,59 +30,58 @@ public class PlayerController : MonoBehaviour
 	Quaternion originalRotation;
 	
 	void Update() {
-		CharacterController controller = GetComponent<CharacterController>();
-		/*transform.Rotate(0, Input.GetAxis("Horizontal") * rotateSpeed, 0);
+				CharacterController controller = GetComponent<CharacterController> ();
+				/*transform.Rotate(0, Input.GetAxis("Horizontal") * rotateSpeed, 0);
 		Vector3 forward = -transform.TransformDirection(Vector3.forward);
 		float curSpeed = speed * Input.GetAxis("Vertical");
 		controller.SimpleMove(forward * curSpeed);*/
-		if (controller.isGrounded) {
-			moveDirection = new Vector3(Input.GetAxis(controlh), 0, Input.GetAxis(controlv));
-			moveDirection = -transform.TransformDirection(moveDirection);
-			moveDirection *= speed;
-			if (Input.GetButton("Jump"))
-				moveDirection.y = jumpSpeed;
+				if (controller.isGrounded) {
+						moveDirection = new Vector3 (Input.GetAxis (controlh), 0, Input.GetAxis (controlv));
+						moveDirection = -transform.TransformDirection (moveDirection);
+						moveDirection *= speed;
+						if (Input.GetButton ("Jump"))
+								moveDirection.y = jumpSpeed;
 			
-		}
-		moveDirection.y -= gravity * Time.deltaTime;
-		controller.Move(moveDirection * Time.deltaTime);
-		if (axes == RotationAxes.MouseXAndY)
-		{
+				}
+				moveDirection.y -= gravity * Time.deltaTime;
+				controller.Move (moveDirection * Time.deltaTime);
+				if (axes == RotationAxes.MouseXAndY) {
 			
-			rotationX += Input.GetAxis("Mouse X") * sensitivityX;
-			rotationY += -Input.GetAxis("Mouse Y") * sensitivityY;
+						rotationX += Input.GetAxis ("Mouse X") * sensitivityX;
+						rotationY += -Input.GetAxis ("Mouse Y") * sensitivityY;
 			
-			rotationX = ClampAngle (rotationX, minimumX, maximumX);
-			rotationY = ClampAngle (rotationY, minimumY, maximumY);
+						rotationX = ClampAngle (rotationX, minimumX, maximumX);
+						rotationY = ClampAngle (rotationY, minimumY, maximumY);
 			
-			Quaternion xQuaternion = Quaternion.AxisAngle (Vector3.up, Mathf.Deg2Rad * rotationX);
-			Quaternion yQuaternion = Quaternion.AxisAngle (Vector3.left, Mathf.Deg2Rad * rotationY);
+						Quaternion xQuaternion = Quaternion.AxisAngle (Vector3.up, Mathf.Deg2Rad * rotationX);
+						Quaternion yQuaternion = Quaternion.AxisAngle (Vector3.left, Mathf.Deg2Rad * rotationY);
 			
-			transform.localRotation = originalRotation * xQuaternion * yQuaternion;
-		}
-		else if (axes == RotationAxes.MouseX)
-		{
-			rotationX += Input.GetAxis("Mouse X") * sensitivityX;
-			rotationX = ClampAngle (rotationX, minimumX, maximumX);
+						transform.localRotation = originalRotation * xQuaternion * yQuaternion;
+				} else if (axes == RotationAxes.MouseX) {
+						rotationX += Input.GetAxis ("Mouse X") * sensitivityX;
+						rotationX = ClampAngle (rotationX, minimumX, maximumX);
 			
-			Quaternion xQuaternion = Quaternion.AxisAngle (Vector3.up, Mathf.Deg2Rad * rotationX);
-			transform.localRotation = originalRotation * xQuaternion;
-		}
-		else
-		{
-			rotationY += -Input.GetAxis("Mouse Y") * sensitivityY;
-			rotationY = ClampAngle (rotationY, minimumY, maximumY);
+						Quaternion xQuaternion = Quaternion.AxisAngle (Vector3.up, Mathf.Deg2Rad * rotationX);
+						transform.localRotation = originalRotation * xQuaternion;
+				} else {
+						rotationY += -Input.GetAxis ("Mouse Y") * sensitivityY;
+						rotationY = ClampAngle (rotationY, minimumY, maximumY);
 			
-			Quaternion yQuaternion = Quaternion.AxisAngle (Vector3.left, Mathf.Deg2Rad * rotationY);
-			transform.localRotation = originalRotation * yQuaternion;
-		}
-		if(Input.GetButtonDown("Fire1"))
-		{
+						Quaternion yQuaternion = Quaternion.AxisAngle (Vector3.left, Mathf.Deg2Rad * rotationY);
+						transform.localRotation = originalRotation * yQuaternion;
+				}
+				if (Input.GetButtonDown ("Fire1")) {
 
-			Instantiate(shu,shus.transform.position,shus.transform.rotation);
+						Instantiate (shu, shus.transform.position, shus.transform.rotation);
 
-			
-			
+				}
+		if (Input.GetKeyDown(KeyCode.LeftShift)) {
+			speed = 4.5f;
 		}
+		else if (Input.GetKeyUp(KeyCode.LeftShift)) {
+			speed = 3.0f;
+		}
+		
 	}
 	
 	void Start ()
---
+++ Unity/Dawn-Of-Gates/Assets/Scripts/Turret.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Turret : MonoBehaviour {
+
+	public Rigidbody bulletPrefab;
+	private Transform target;
+	public Projectile bullet;
+	private float nextFire;
+
+	
+	void OnTriggerEnter(Collider otherCollider) {
+
+			target = otherCollider.transform;
+			StartCoroutine ("Fire");
+		
+	}
+	
+	void  OnTriggerExit(Collider otherCollider) {
+
+			target = null;
+			StopCoroutine("Fire");
+
+	}
+	
+	IEnumerator Fire()
+	{
+		while (target != null)
+		{
+			nextFire = Time.time + 0.5f;
+			while (Time.time < nextFire)
+			{
+
+
+				yield return new WaitForEndOfFrame();
+			}
+
+
+			bullet = Instantiate(bulletPrefab, transform.position, transform.rotation) as Projectile;
+
+		}
+	}
+
+}
