a7a1244f90c61636234be643a57b8c8c57c5be42
Steven Lariau <obs145628@gmail.com>
2015-01-30 18:35:53 +0100

    Rangement unstable

---
+++ Unity/tests/obs/Assets/Scripts/Characters/Stats/CharacterStats.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterStats : MonoBehaviour {
+
+	public int maxLife = 100;
+	public float dieWait = 0.5f;
+	
+	
+	private int life;
+	public int Life {
+		get { return life; }
+		set {
+			if(value < 0) {
+				life = 0;
+				kill();
+			}
+			else if(value > maxLife) {
+				life = maxLife;
+			}
+			else {
+				life = value;
+			}
+		}
+	}
+	
+	public void kill() {
+		Destroy (gameObject, dieWait);
+	}
+	
+	
+	void Start () {
+		Life = maxLife;
+	}
+}
---
+++ Unity/tests/obs/Assets/Scripts/Characters/Stats/PlayerStats.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerStats : CharacterStats {
+
+	public int maxEnergy = 500;
+	public float lifeRegenDelay = 2f;
+	public int lifeRegenValue = 50;
+	public float energyRegenDelay = 2f;
+	public int energyRegenValue = 50;
+
+	private float lastLifeRegen;
+	private float lastEnergyRegen;
+
+	private int energy;
+	public int Energy {
+		get { return energy; }
+		set {
+			if(value < 0) {
+				energy = 0;
+			}
+			else if(value > maxEnergy) {
+				energy = maxEnergy;
+			}
+			else {
+				energy = value;
+			}
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		Life = maxLife;
+		Energy = maxEnergy;
+		lastLifeRegen = 0f;
+		lastEnergyRegen = 0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		lastLifeRegen += Time.deltaTime;
+		lastEnergyRegen += Time.deltaTime;
+
+		if (lastLifeRegen > lifeRegenDelay) {
+				Life += lifeRegenValue;
+				lastLifeRegen -= lifeRegenDelay;
+		}
+		if (lastEnergyRegen > energyRegenDelay) {
+			Energy += energyRegenValue;
+			lastEnergyRegen -= energyRegenDelay;
+		}
+
+
+
+	}
+}
