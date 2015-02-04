a91925a1ec83e608d0bf3daff31b887bfd0ce398
wxbn <wxbn@live.fr>
2015-02-04 19:52:46 +0100

    Barre de vie
    Hyper long à faire argh
    
    Pour tester demarrez le jeu puis selectionnez GameObject et faites
    varier la valeur Life.

---
+++ Unity test/Lifebar/Assets/MyGui.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MyGui : MonoBehaviour {
+	public Texture outline;
+	public Texture lifebar;
+	public Material outline_mat;
+	public Material lifebar_mat;
+	public float Life;
+
+	void OnGUI()
+	{
+		if (Life > 1)
+		{
+			Life = 1;
+		}
+		Graphics.DrawTexture (new Rect(10, 10, 493, 76), outline, outline_mat);
+		lifebar_mat.SetColor ("_Life", new Color (1.0f - Life, Life, 0f, 1));
+		Graphics.DrawTexture (new Rect(21, 22, 470 * Life, 50), lifebar, lifebar_mat);
+	}
+}
