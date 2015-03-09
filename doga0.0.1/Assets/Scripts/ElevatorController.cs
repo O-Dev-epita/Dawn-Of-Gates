48c595cbb3dfb37211ae8143ea9c1fdba2064d49
Steven Lariau <obs145628@gmail.com>
2015-03-09 17:53:02 +0100

    hhg

---
+++ doga0.0.1/Assets/Scripts/ElevatorController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ElevatorController : MonoBehaviour {
+
+	public GameObject leftDoor;
+	public GameObject rightDoor;
+	
+	void OnTriggerEnter(Collider other)
+	{
+		leftDoor.animation.Play ("porteg");
+		rightDoor.animation.Play ("ported");
+	}
+
+	void OnTriggerExit(Collider other)
+	{
+		leftDoor.animation.Play ("invporteg");
+		rightDoor.animation.Play ("invported");
+	}
+
+	void Update ()
+	{
+	
+	}
+}
---
+++ doga0.0.1/Assets/Scripts/Teleportation.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Teleportation : MonoBehaviour {
+	
+	public GameObject otherPortal;
+	public Texture openedPortalTexture;
+	public Texture closedPortalTexture;
+	public GameObject animatedportal;
+
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	bool teleportOk() {
+		GameState gameState = GameState.Instance;
+		return gameState.leftPortalOpen && gameState.rightPortalOpen;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		renderer.material.mainTexture = teleportOk() ? openedPortalTexture : closedPortalTexture;
+	}
+	
+	void OnTriggerEnter(Collider collider)
+	{
+		Debug.Log(collider.name);
+		if(this.teleportOk())
+		{
+			collider.transform.position = otherPortal.transform.position;
+			Camera.main.transform.rotation = otherPortal.transform.rotation;
+			collider.transform.position += Camera.main.transform.TransformDirection(Vector3.forward * 2f);
+			//collider.transform.rotation = otherPortal.transform.rotation;
+
+		}	
+	}
+
+
+}
