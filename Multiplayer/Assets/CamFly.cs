19b18dfa608be6c3c85051ce2501c7135e8d8b7f
wxbn <wxbn@live.fr>
2015-06-18 11:23:22 +0200

    carte multi + code pour multi
    Le code CamFly fonctionne, le code pour le multi pas encore

---
+++ Multiplayer/Assets/CamFly.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CamFly : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.mousePosition.x < Screen.width * 0.2)
+		{
+			transform.position = new Vector3(transform.position.x - 0.1f, transform.position.y, transform.position.z);
+		}
+
+		if (Input.mousePosition.x > Screen.width * 0.8)
+		{
+			transform.position = new Vector3(transform.position.x + 0.1f, transform.position.y, transform.position.z);
+		}
+
+		if (Input.mousePosition.y < Screen.height * 0.2)
+		{
+			transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z  - 0.1f);
+		}
+
+		if (Input.mousePosition.y > Screen.height * 0.8)
+		{
+			transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + 0.1f);
+		}
+	
+	}
+}
---
+++ Multiplayer/Assets/Multiplayer.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Multiplayer : MonoBehaviour {
+
+	bool connected;
+	double starttime;
+
+	// Use this for initialization
+	void Start () {
+		connected = false;
+		Network.Connect("127.0.0.1", 80);
+		starttime = Network.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(connected == false && Network.time > starttime + 4.0)
+		{
+			Network.Disconnect();
+			MasterServer.UnregisterHost();
+			Debug.Log("An error happend while trying to connect as client, starting server now");
+
+
+			Network.InitializeServer(2, 80, false);
+		}
+	}
+
+	void OnConnectedToServer() {
+		connected = true;
+		Debug.Log("Successfully onnected to server");
+	}
+}
