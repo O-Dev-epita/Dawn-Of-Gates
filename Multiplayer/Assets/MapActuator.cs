2038bcaaedae93eeeaac976baebe3258239debf3
wxbn <wxbn@live.fr>
2015-06-22 16:29:49 +0200

    Carte + objets + amélioration multi
    J'ai réussi a transférer une info sur le réseau, je vous raconterais
    comment faire

---
+++ Multiplayer/Assets/MapActuator.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MapActuator : MonoBehaviour {
+
+	int var = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnSerializeNetworkView(BitStream stream, NetworkMessageInfo info)
+    {
+        if (stream.isWriting)
+        {
+            int tosend = var;
+            stream.Serialize(ref tosend);
+        }
+        else
+        {
+        	int received = 0;
+            stream.Serialize(ref received);
+            var = received;
+            Debug.Log(var);
+        }
+    }
+}
--- Multiplayer/Assets/Multiplayer.cs
+++ Multiplayer/Assets/Multiplayer.cs
@@ -4,30 +4,36 @@ using System.Collections;
 public class Multiplayer : MonoBehaviour {
 
 	bool connected;
+	bool tryserver;
 	double starttime;
 
 	// Use this for initialization
 	void Start () {
 		connected = false;
-		Network.Connect("127.0.0.1", 80);
+		Network.Connect("127.0.0.1", 25000);
 		starttime = Network.time;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(connected == false && Network.time > starttime + 4.0)
+		if(connected == false && Network.time > starttime + 4.0 && tryserver == false)
 		{
 			Network.Disconnect();
 			MasterServer.UnregisterHost();
 			Debug.Log("An error happend while trying to connect as client, starting server now");
 
 
-			Network.InitializeServer(2, 80, false);
+			Network.InitializeServer(5, 25000, false);
+			tryserver = true;
 		}
 	}
 
 	void OnConnectedToServer() {
 		connected = true;
 		Debug.Log("Successfully onnected to server");
+	}
+
+	void OnServerInitialized() {
+		Debug.Log("Server initialized");
 	}
 }
